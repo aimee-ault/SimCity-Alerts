07371a42e92ca3c0b8b2873da3cdb40cab3e7dee
Aimee Ault <aimeeault@gmail.com>
2012-10-08 23:36:12 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+    $styles = array('simcity-alerts.css'); 
+    $scripts = array('simcity-alerts.js');
+?>
+<? include($_SERVER['DOCUMENT_ROOT'].'/includes/head.php'); ?>
+<a data-role="simcity" data-title="Simcity-Alerts.js" data-message="For when you really want to communicate information to your audience channeled through the voice of everyone's favorite green-haired mayoral advisor, Dr. Wright." class="btn" data-toggle="modal">Launch demo modal</a>
+<? include($_SERVER['DOCUMENT_ROOT'].'/includes/foot.php'); ?>
