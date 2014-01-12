e5b3800ca9cc453c383f3945a870cc7dd13d758d
Aimee Ault <aimeeault@gmail.com>
2014-01-12 08:54:27 -0500

    Why was this using .attr?

--- simcity-alerts.js
+++ simcity-alerts.js
@@ -3,7 +3,7 @@ $(function (e){
     
     $('*[data-role=simcity]').on("click", function (e){
        var $this = $(this),
-           $modal = $('<div class="simcity-modal"><div class="simcity-modal-inner"><h3>'+$this.attr('data-title')+'</h3><p>'+$this.attr('data-message')+'</p><div id="desk"></div><div id="floor"></div><div id="dr-wright"></div><div class="window" id="window2"></div><div class="window"></div></div></div>'),
+           $modal = $('<div class="simcity-modal"><div class="simcity-modal-inner"><h3>'+$this.data('title')+'</h3><p>'+$this.data('message')+'</p><div id="desk"></div><div id="floor"></div><div id="dr-wright"></div><div class="window" id="window2"></div><div class="window"></div></div></div>'),
            h = $modal.children('h3').outerHeight() + $modal.children('p').outerHeight() + 140,
            $background = $('<div id="simcity-modal-background" style="height: 100%; width: 100%; background: rgba(0,0,0,0.4); position: fixed; left: 0; top: 0; z-index: 1"></div>');         
            $modal.css({'height': h, 'z-index': 5000}).show().animate({
