87718c94e0358b1e03b397f90aabad16ae57bd73
Aimee Ault <aimeeault@gmail.com>
2014-01-12 08:59:53 -0500

    Update simcity-alerts.js
    Use cached stuff and chain.

--- simcity-alerts.js
+++ simcity-alerts.js
@@ -6,20 +6,24 @@ $(function (e){
            $modal = $('<div class="simcity-modal"><div class="simcity-modal-inner"><h3>'+$this.data('title')+'</h3><p>'+$this.data('message')+'</p><div id="desk"></div><div id="floor"></div><div id="dr-wright"></div><div class="window" id="window2"></div><div class="window"></div></div></div>'),
            h = $modal.children('h3').outerHeight() + $modal.children('p').outerHeight() + 140,
            $background = $('<div id="simcity-modal-background" style="height: 100%; width: 100%; background: rgba(0,0,0,0.4); position: fixed; left: 0; top: 0; z-index: 1"></div>');         
-           $modal.css({'height': h, 'z-index': 5000}).show().animate({
-               width: "420px",
-               left: "-210px",
-           }, 500);
-        $('body').append($modal);
-        $('body').append($background);
+           $modal.css({'height': h, 'z-index': 5000})
+                 .show()
+                 .animate({
+                    width: "420px",
+                    left: "-210px",
+                }, 500);
+        
+        $('body').append($modal)
+                 .append($background);
+
         snd.play();
+
         $('.simcity-modal *').delay(400).fadeIn();
-        $('#simcity-modal-background').click(function (e){
+        $modal.on('click', '#simcity-modal-background', function (e) {
             $modal.fadeOut();
             $background.fadeOut().remove();
             snd.pause();
             snd.currentTime=0;
         });
     });
-
 });
